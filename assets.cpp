cb2d7c64253aa0bf1111edd19e8c6c2ffd03fa21
FJ05 <fredrikfilip.fj@gmail.com>
2025-05-07 15:32:24 +0200

    update assets.cpp

--- assets.cpp
+++ assets.cpp
@@ -1,9 +1,43 @@
 #include <string>
+#include <vector>
+
+class Sport;  // Forward declaration
+
+class Division {
+public:
+    int ageFrom = 0;
+    int ageTo = 0;
+    std::string name;
+    std::string desc;
+    std::string optDesc;
+
+    Division() = default;
+    // Const for Division
+    Division(int from, int to, std::string n, std::string d, std::string opt = "")
+        : ageFrom(from), ageTo(to), name(n), desc(d), optDesc(opt) {}
+};
+
+class Score {
+public:
+    Sport* sport = nullptr;
+    std::string scoreStr;
+    Division division;
+};
+
+class Competitor {
+public:
+    std::string name;
+    std::string surname;
+    int age = 0;
+    char sex = 'N';
+    std::string club;
+    std::vector<Score> scoreArr;
+};
 
 class Sport {
-    public:
-        std::string name;
-        // comp Arr
-        // div arr
-        std::string unit;
+public:
+    std::string name;
+    std::vector<Competitor> competitorArr;
+    std::vector<Division> divisionArr;
+    std::string unit;
 };
