1fdf9cb091ee1ad3e8cf04b3a9f37f861b0f3a69
FJ05 <fredrikfilip.fj@gmail.com>
2025-05-14 17:19:55 +0200

    Update comments in inputhandler.cpp

--- inputHandler.cpp
+++ inputHandler.cpp
@@ -17,13 +17,15 @@ int CreateSport(){
     cout << "\nWhat unit will be used for the sport? ex (s, m, min, feet, inches, cm)";
     cin >> units;
 
-
     cout << "\nDo you want " << name << " as the name, " << arenaSize << " As the max host of the arena and \n" << units << " as the units of the sport? (y/n)";
     cin >> boolian;
 
     if(tolower(boolian) == 'y'){
         // Save the name
 
+        // Add Division
+
+        // Add Competitor
     }
     else{
         // Exit to main
