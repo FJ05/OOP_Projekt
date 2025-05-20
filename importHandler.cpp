f14d9ec7bd67768dd8e8567dc6a2b4f062c040f7
LeoLundqvist <leolundqvist05@gmail.com>
2025-05-20 17:27:57 +0200

    working on importHandler

--- importHandler.cpp
+++ importHandler.cpp
@@ -3,39 +3,60 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include "assets.h"
+
 using namespace std;
 
 class ImportSystem 
 {
 public:
-    void ImportValues()
+    void ImportValues(vector<Competitor> *competitors, vector<Score> *scores)
     {
         cout << "start importvalues" << endl;
         ifstream dbFile;
         string line = "";
-        dbFile.open("db.csv");
+        dbFile.open("DB/compIndex.csv");
         getline(dbFile, line);
+        int lineNumber=0;
+        
+        Score s;
 
         while(getline(dbFile, line))
         {
             stringstream ss(line);
-            
             string substr;
             vector<string> v;
+            Competitor c;
+
+            cout << "test 1" << endl;
 
             while (getline(ss,substr, ',')) //splits up the line into substr and stores values in v
             {
                 v.push_back(substr); 
-                //cout << substr << endl;
+                cout << substr << endl;
+            }
+            cout << "test 2" << endl;
 
+            cout << "test 3" << endl;
+
+            c.name = v[0];
+            c.surname = v[1];
+            c.age = stoi(v[2]);
+            c.sex = v[3][0];
+            c.club = v[4];
+
+            for(int i = 5; i<v.size(); i++)
+            {
+                s.scoreStr = v[i];
+                //cout <<"score:" << i-4 << ": "<< v[i] << endl;
             }
 
-            //save name = v[0];
-            //save surname = v[1];
-            //etc... in order of the csv file
-            v.clear();
+            competitors->push_back(c);
 
+            v.clear();
+            cout << "name:" << c.name << endl;
             cout << "Full line:" << line << endl;
+            lineNumber++;
         }
         dbFile.close();
     }
